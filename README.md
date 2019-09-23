33274e6efc8a0c4f709217d26ad1dbfeffe60533
tdude0175 <tdude0175@gmail.com>
2019-09-23 13:11:57 -0500

    graded 2019-09-23

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # python_debugging
-
+#### You got most of them correct, Need to comment code, Nicely done just comment code and complete work. 3/5 Score:3/4
 All of the questions 1 through 10 has bugs in it. Fix each of the questions using the debugger and reading error messages.
--- question7.py
+++ question7.py
@@ -2,7 +2,7 @@
 # Create the class Books with name, rating, genre, and author properties/attributes. Create a class method that will change the rating of the book. Outside of the class, create three objects of the class Book and put them in an array. Lastly, USING THE ARRAY print only the names of the books using any method we’ve learned in class.
 
 class Books:
-    def _init_(self, name, rating, genre, author):
+    def _init_(self, name, rating, genre, author): #you need two underscores on both sides of init
         self.name = name
         self.rating = rating
         self.genre = genre
@@ -18,4 +18,4 @@ book3 = Books("Programmng HTML", 10, "Non-Fiction", "Erin")
 bookArray = [book1, book2, book3]
 
 for eachBook in bookArray:
-    print(eachBook.name())
+    print(eachBook.name()) #you don't need parenthesis at the end of name only to define name
